1609b9270083080926c1f044c68cbdf52c8e7fbe
unknown <renjithjr2912@gmail.com>
2022-10-23 23:09:43 +0530

    added search mock page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
 import Container from "./components/container";
-import NewsList from "./pages/newslist";
 import "./App.css";
 
 function App() {
   return (
     <React.Fragment>
       <Container />
-      
     </React.Fragment>
   );
 }
--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -1,5 +1,118 @@
+import {
+  Avatar,
+  Divider,
+  Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Pagination,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React from "react";
+
 const Search = () => {
-  return <div>Search page</div>;
+  const [page, setPage] = React.useState(1);
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="flex-start"
+      alignItems="flex-start"
+    >
+      <Grid
+        item
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item>
+          <TextField />
+        </Grid>
+        <Grid item>
+          <IconButton />
+        </Grid>
+      </Grid>
+      <Grid item md={12}>
+        <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Ali Connors
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Summer BBQ"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    to Scott, Alex, Jennifer
+                  </Typography>
+                  {" — Wish I could come, but I'm out of town this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Oui Oui"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Sandra Adams
+                  </Typography>
+                  {" — Do you have Paris recommendations? Have you ever…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+        </List>
+      </Grid>
+      <Grid container item md={12} justifyContent="center" alignItems="center">
+        <Pagination count={10} page={page} onChange={handleChange} />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Search;
